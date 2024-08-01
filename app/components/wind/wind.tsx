36f289b75c9e04011152c645c33d1ff1eed31cc7
Rosanne Saparamadu <rosanne.thms@gmail.com>
2024-08-02 02:08:56 +1200

    updated wind speed from m/s to km/h

--- app/components/wind/wind.tsx
+++ app/components/wind/wind.tsx
@@ -8,13 +8,14 @@ import React from 'react';
 
 function Wind() {
     const { forecast } = UserGlobalContext();
-    const windSpeed = forecast?.wind?.speed;
+    let windSpeed = forecast?.wind?.speed;
     const windDir = forecast?.wind?.deg;
     if (!windSpeed || !windDir) {
         return (
             <Skeleton className='h-[12rem] w-full' />
         );
     }
+    windSpeed = windSpeed * 3.6
     return (
         <div className='pt-6 px-4 h-[12rem] border rounded-lg flex flex-col gap-3 dark:bg-dark-grey shadow-sm dark:shadow-none'>
             <h2 className='flex items-center gap-2 font-medium'>{wind}Wind</h2>
@@ -34,7 +35,7 @@ function Wind() {
                         height={11} 
                     />
                 </div>
-                <p className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-xs dark:text-white font-medium'>{Math.round(windSpeed)} m/s</p>
+                <p className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-xs dark:text-white font-medium'>{Math.round(windSpeed)} km/h</p>
             </div>
         </div>
     );
